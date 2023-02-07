f587ae4e1501d2762669f4cc27eaa334d7cb5d04
Md. Al amin <alamincsecu@gmail.com>
2023-02-07 08:36:47 +0600

    added largest number

--- largestNumber.js
+++ largestNumber.js
@@ -1,3 +1,12 @@
-function findMax(numbers){
-                          
+function findMax(numbers) {
+    if (numbers[0] > numbers[1] && numbers[0] > numbers[2]) {
+        return numbers[0];
+    }
+    else if (numbers[1] > numbers[0] && numbers[1] > numbers[2]) {
+        return numbers[1];
+    } else {
+        return numbers[2];
+    }
 }
+const maxNum = findMax([55, 10, 95]);
+console.log(maxNum);
